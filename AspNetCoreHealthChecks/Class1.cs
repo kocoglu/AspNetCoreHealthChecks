41753f463e65b20fabfadf7b00264fdd39cbc41b
Mustafa KOCOGLU <mustafa.kocoglu@santsg.com>
2020-09-17 12:50:16 +0300

    Project created

---
+++ AspNetCoreHealthChecks/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AspNetCoreHealthChecks
+{
+    public class Class1
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks()
+                        .AddCheck("Health Check UI - Zero", () => HealthCheckResult.Healthy())
+                        .AddCheck("Health Check UI - 001", () => HealthCheckResult.Healthy(), tags: new[] { "Tag001" })
+                        .AddCheck("Health Check UI - 002", () => HealthCheckResult.Healthy(), tags: new[] { "Tag002" })
+                        .AddCheck("Health Check UI - 003", () => HealthCheckResult.Healthy(), tags: new[] { "Tag003" })
+                        .AddCheck("Health Check UI - 004", () => HealthCheckResult.Healthy(), tags: new[] { "Tag001", "Tag002", "Tag003" })
+                        .AddCheck("Health Check UI - 005", () => new Random().Next(1, 4) % 2 == 0 ? HealthCheckResult.Healthy() : HealthCheckResult.Unhealthy());
+
+            services
+                .AddHealthChecksUI(setup =>
+                {
+                    setup.MaximumHistoryEntriesPerEndpoint(50);
+                    setup.SetEvaluationTimeInSeconds(10);
+
+                    setup.AddHealthCheckEndpoint("Health Check Zero", "https://localhost:44339/hc");
+                    setup.AddHealthCheckEndpoint("Health Check 000", "https://localhost:44339/hc000");
+                    setup.AddHealthCheckEndpoint("Health Check 001", "https://localhost:44339/hc001");
+                    setup.AddHealthCheckEndpoint("Health Check 002", "https://localhost:44339/hc002");
+                    setup.AddHealthCheckEndpoint("Health Check 003", "https://localhost:44339/hc003");
+                })
+                .AddSqliteStorage("Data Source = SQLite.db");
+        }
+    }
+}
---
+++ AspNetCoreHealthChecks/Class2.cs
@@ -0,0 +1,49 @@
+﻿using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace AspNetCoreHealthChecks
+{
+    public class Class2
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHealthChecksUI(options => {
+                options.ApiPath = "/healthchecks-api";
+                options.UIPath = "/healthchecks-ui";
+            });
+
+
+            app.UseHealthChecks("/hc", new HealthCheckOptions()
+            {
+                Predicate = (check) => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks("/hc000", new HealthCheckOptions()
+            {
+                Predicate = (check) => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks("/hc001", new HealthCheckOptions()
+            {
+                Predicate = (check) => check.Tags.Contains("Tag001"),
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks("/hc002", new HealthCheckOptions()
+            {
+                Predicate = (check) => check.Tags.Contains("Tag002"),
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+
+            app.UseHealthChecks("/hc003", new HealthCheckOptions()
+            {
+                Predicate = (check) => check.Tags.Contains("Tag003"),
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+        }
+    }
+}
